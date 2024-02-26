42a46a435066ea0cf8332f330b1c40767ee1abe5
Gabriel Alexandre Negri Machado <bilbor8@gmail.com>
2024-02-26 11:06:06 -0300

    mudanças no main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from api.dogs_view import create_dogs_blueprint
 
 
@@ -12,9 +12,24 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/aboutus')
+dogs = []
+dict_dogs = []
+
+
+@app.route('/aboutus',  methods=["GET", "POST"])
 def aboutus():
-    return render_template("aboutus.html")
+    if request.method == "POST":
+        if request.form.get("dog") and request.form.get("tutor"):
+            dict_dogs.append({"dog": request.form.get("dog"), "tutor": request.form.get("tutor")})
+    return render_template("aboutus.html", dict_dogs=dict_dogs)
+
+
+@app.route('/indexcurso',  methods=["GET", "POST"])
+def indexcurso():
+    if request.method == "POST":
+        if request.form.get("dogs"):
+            dogs.append(request.form.get("dogs"))
+    return render_template("indexcurso.html", dogs=dogs)
 
 
 app.run()
