d594a8640a49f53904577dcd112460e3660c105a
Luís Gustavo Oliveira da Silva <luis.gustavo.ods@gmail.com>
2024-03-25 10:21:36 -0300

    Commit login forms
    Commit login forms

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
-from main import app
+
 
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, IntegerField, DateTimeField 
+from wtforms import StringField, SubmitField, PasswordField, IntegerField, DateTimeField, DateField
 from wtforms.validators import DataRequired
-
+from flask_sqlalchemy  import SQLAlchemy
 
 
 
@@ -16,15 +16,17 @@ class LoginForm(FlaskForm):
     
 
 
-#Registro que qualquer usuario pode fazer 
+#Cadastro dos usuários na página principal, ou seja, livre
 class UserRegisterForm(FlaskForm):
 
     regCPF           =  StringField    ("CPF", validators=[DataRequired()])
     regName          =  StringField    ("Nome", validators=[DataRequired()])
     regEmail         =  StringField    ("E-mail", validators=[DataRequired()])
     regPhone         =  StringField    ("Telefone")
-    regPassword      =  StringField    ("Senha", validators=[DataRequired()])    
-    regDateRegister  =  DateTimeField  ("Data")
+    regPassword      =  StringField    ("Senha", validators=[DataRequired()])
+    regDateRegister  =  DateField      ("Data")
     regState         =  StringField    ("Estado")
     regCity          =  StringField    ("Cidade")
+    regAddress       =  StringField    ("Endereço")
     regZipCode       =  StringField    ("CEP")
+    submit           =  SubmitField    ("Cadastrar")
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask_wtf import FlaskForm
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 from wtforms import StringField, SubmitField, PasswordField, validators
 from flask_sqlalchemy import SQLAlchemy
 from sklearn.preprocessing import StandardScaler
@@ -17,6 +16,7 @@ from sklearn.impute import SimpleImputer
 
 app = Flask("Adoption")
 app.config['JSON_SORT_KEYS'] = False
+app.config['PERMANENT_SESSION_LIFETIME'] = 86400
 app.register_blueprint(create_dogs_blueprint())
 
 
@@ -57,6 +57,8 @@ class Dogs(db.Model):
 
 class User(db.Model):
 
+
+
     cpf = db.Column(db.String(11), primary_key=True, nullable=False)
     name = db.Column(db.String(50), primary_key=True, nullable=False)
     email = db.Column(db.String(40), nullable=False, unique=True)
@@ -72,6 +74,20 @@ class User(db.Model):
     def __repr__(self):
         return f'Name: {self.name}'
 
+    def __init__(self, cpf, name, email, phone, password, user_level, date_register, city, state,
+                 address, zipCode):
+        self.cpf= cpf
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.password = password
+        self.user_level = user_level
+        self.date_register = date_register
+        self.city = city
+        self.state = state
+        self.address = address
+        self.zipCode = zipCode
+
 
 
 #routes
@@ -80,14 +96,33 @@ class User(db.Model):
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    name = session.get("name")
+    return render_template("index.html", name=name)
 
-@app.route("user/register", methods=["POST"])
+@app.route("/user/register", methods=["POST", "GET"])
 def user_register():
 
     form = UserRegisterForm()
 
+    if form.validate_on_submit():
 
+        cpf = form.regCPF.data
+        name = form.regName.data
+        email = form.regEmail.data
+        phone = form.regPhone.data
+        password = form.regPassword.data
+        date = form.regDateRegister.data
+        state = form.regState.data
+        city = form.regState.data
+        address = form.regAddress.data
+        zipCode = form.regZipCode.data
+
+        user = User(cpf=cpf, name=name, email=email, phone=phone, password=password, user_level = 0,
+                    date_register=date, city=city, state=state, address=address, zipCode=zipCode)
+
+        db.session.add(user)
+        db.session.commit()
+        return "User added successfully!!!"
 
     return render_template("register.html", form=form)
 
@@ -99,7 +134,28 @@ def login():
     if form.validate_on_submit():
 
         name = form.loginName.data
-        password = form.loginName.data
+        password = form.loginPwd.data
+
+        users = User.query.all()
+
+        for user in users:
+
+            print(f"Typed: {password}\nPass {user.password}")
+
+            if user.password == password:
+
+                session["name"] = user.cpf
+                session.permanent = True
+                return redirect('/')
+            else:
+
+                redirect(url_for('login'))
+
+
+
+
+
+
 
         # Login with SQLAlchemy
 
@@ -202,8 +258,9 @@ if __name__ == "__main__":
         db.create_all()
         df = pd.read_sql_query("SELECT * FROM dogs", db.engine)
     app.config['SECRET_KEY'] = key
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
     app.run(debug=True)
-    app.config['SECRET_KEY'] = key
     app.secret_key = key
     csrf = CSRFProtect(app)
     csrf.init_app(app)
