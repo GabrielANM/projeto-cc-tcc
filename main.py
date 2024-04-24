668dc4c1309ddc28576d6dc9174bea4dfaed601d
Gabriel Alexandre Negri Machado <bilbor8@gmail.com>
2024-04-24 09:17:37 -0300

    remove csrf

--- main.py
+++ main.py
@@ -311,5 +311,5 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.run()
 app.secret_key = key
-csrf = CSRFProtect(app)
-csrf.init_app(app)
+# csrf = CSRFProtect(app)
+# csrf.init_app(app)
